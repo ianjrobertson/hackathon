92113f50924406873f726b7cb709aba6f4fb72ce
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2025-03-22 11:36:38 -0600

    Fixed email subject

--- src/home/contactUs.jsx
+++ src/home/contactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
                 <div className="text-center">
                     <p className="">Send us an email and let's get in touch!</p>
                     <button className="text-accent bg-primary hover:bg-neutral px-1 py-1 rounded my-2">
-                        <a href="mailto:reproute.hackathon@gmail.com?subject=Beta%Testing&body=Hello,%20I%20have%20a%20question%20about...">RepRoute</a>
+                        <a href="mailto:reproute.hackathon@gmail.com?subject=Beta%20Testing&body=Hello,%20I%20have%20a%20question%20about...">RepRoute</a>
                     </button>
                 </div>
             </div>
